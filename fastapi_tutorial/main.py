6321709a7a187023968d14afb134a5f59b71dca4
JulianTsugami <julian.informatics@gmail.com>
2022-04-30 12:51:26 +0900

    Query parameter list

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -6,10 +6,6 @@ app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(
-    q: Optional[str] = Query(None, min_length=3, max_length=50, regex="^fixedquery$")
-):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+async def read_items(q: Optional[list[str]] = Query(None)):
+    query_items = {"q": q}
+    return query_items
