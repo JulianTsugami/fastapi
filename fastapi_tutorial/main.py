bfbe03b0298f74d878f0edf83b08f482c45a0c51
JulianTsugami <julian.informatics@gmail.com>
2022-04-30 12:48:45 +0900

    Query Parameters and String Validations

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -1,19 +1,15 @@
 from typing import Optional
 
-from fastapi import FastAPI
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+@app.get("/items/")
+async def read_items(
+    q: Optional[str] = Query(None, min_length=3, max_length=50, regex="^fixedquery$")
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
